72c9534aa4825b59fcad94f40131c278c6db5597
superfer2002 <fporcel2002@gmail.com>
2021-11-02 15:36:52 +0000

    Declaracion de pins de leds y botones

--- fase1.c
+++ fase1.c
@@ -11,4 +11,14 @@
 #include <linux/interrupt.h>  
 #include <linux/kobject.h>    
 #include <linux/time.h>       
-#define  DEBOUNCE_TIME 200    
+#define  DEBOUNCE_TIME 200 
+
+MODULE_LICENSE("GPL");
+
+static unsigned int gpioRed = 20
+static unsigned int gpioBlue = 16
+
+static unsigned int gpioButton1 = 26
+static unsigned int gpioButton2 = 19
+static unsigned int gpioButton3 = 13
+static unsigned int gpioButton4 = 21
