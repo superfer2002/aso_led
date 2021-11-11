dd49edb694aa32ad83fb149a3d5469dc8754ecbd
superfer2002 <fporcel2002@gmail.com>
2021-11-11 08:59:22 +0000

    Llamadas a scripts para cada boton

--- fase1.c
+++ fase1.c
@@ -12,7 +12,7 @@
 #include <linux/interrupt.h>  
 #include <linux/kobject.h>    
 #include <linux/time.h>       
-#define  DEBOUNCE_TIME 200 
+#include <linux/unistd.h>
 
 MODULE_LICENSE("GPL");
 
@@ -42,7 +42,6 @@ static irq_handler_t ebbgpio_irq_handler1(unsigned int irq, void *dev_id, struct
 static irq_handler_t ebbgpio_irq_handler2(unsigned int irq, void *dev_id, struct pt_regs *regs);
 static irq_handler_t ebbgpio_irq_handler3(unsigned int irq, void *dev_id, struct pt_regs *regs);
 static irq_handler_t ebbgpio_irq_handler4(unsigned int irq, void *dev_id, struct pt_regs *regs);
-
 
 static int __init ebbgpio_init(void){
 
@@ -153,6 +152,11 @@ static irq_handler_t ebbgpio_irq_handler1(unsigned int irq, void *dev_id, struct
 	gpio_set_value(gpioRed, redOn);
 	numberPresses1++;  //aumentamos el contador
 	printk("Button 1 pressed!");
+
+	char *argv[] = {"/home/pi/practica/aso_led/button1.sh", NULL};
+	char *envp[] = {"HOME=/", NULL};
+	call_usermodehelper(argv[0], argv, envp, UMH_NO_WAIT);
+
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
@@ -161,6 +165,11 @@ static irq_handler_t ebbgpio_irq_handler2(unsigned int irq, void *dev_id, struct
 	gpio_set_value(gpioRed, redOn);
 	printk("Button 2 pressed!");
 	numberPresses2++;  //aumentamos el contador
+
+	char *argv[] = {"/home/pi/practica/aso_led/button2.sh", NULL};
+	char *envp[] = {"HOME=/", NULL};
+	call_usermodehelper(argv[0], argv, envp, UMH_NO_WAIT);
+
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
@@ -169,6 +178,11 @@ static irq_handler_t ebbgpio_irq_handler3(unsigned int irq, void *dev_id, struct
 	gpio_set_value(gpioBlue, blueOn);
 	printk("Button 3 pressed!");
 	numberPresses3++;  //aumentamos el contador
+
+	char *argv[] = {"/home/pi/practica/aso_led/button3.sh", NULL};
+	char *envp[] = {"HOME=/", NULL};
+	call_usermodehelper(argv[0], argv, envp, UMH_NO_WAIT);
+
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
@@ -177,6 +191,11 @@ static irq_handler_t ebbgpio_irq_handler4(unsigned int irq, void *dev_id, struct
 	gpio_set_value(gpioBlue, blueOn);
 	printk("Button 4 pressed!");
 	numberPresses4++;  //aumentamos el contador
+
+	char *argv[] = {"/home/pi/practica/aso_led/button4.sh", NULL};
+	char *envp[] = {"HOME=/", NULL};
+	call_usermodehelper(argv[0], argv, envp, UMH_NO_WAIT);
+
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
